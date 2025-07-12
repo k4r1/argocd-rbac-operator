9fd64ce8f2fa0e155c0d5a4ba8e8b431217c0af7
Karl Taylor <16408267+k4r1@users.noreply.github.com>
2025-07-12 19:25:08 +0800

    Test

--- internal/controller/argocdrole_controller.go
+++ internal/controller/argocdrole_controller.go
@@ -121,7 +121,7 @@ func (r *ArgoCDRoleReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 				if err := r.Client.Status().Update(ctx, &role); err != nil {
 					r.Log.Error(err, "Failed to update ArgoCDRole %s status", req.Name)
 				}
-				return ctrl.Result{}, err
+				return ctrl.Result{Requeue: true, RequeueAfter: time.Second}, err
 			}
 		}
 
@@ -158,7 +158,7 @@ func (r *ArgoCDRoleReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 	if err := r.Client.Status().Update(ctx, &role); err != nil {
 		r.Log.Error(err, "Failed to update ArgoCDRole %s status", req.Name)
 	}
-	return ctrl.Result{RequeueAfter: time.Minute * 10}, nil
+	return ctrl.Result{RequeueAfter: time.Second}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
--- internal/controller/argocdrolebinding_controller.go
+++ internal/controller/argocdrolebinding_controller.go
@@ -123,7 +123,7 @@ func (r *ArgoCDRoleBindingReconciler) Reconcile(ctx context.Context, req ctrl.Re
 			if err := r.Client.Status().Update(ctx, &rb); err != nil {
 				r.Log.Error(err, "Failed to update ArgoCDRoleBinding %s status", req.Name)
 			}
-			return ctrl.Result{}, err
+			return ctrl.Result{RequeueAfter: time.Second}, err
 		}
 
 		r.Log.Info("Reconciling RBAC ConfigMap")
@@ -146,7 +146,7 @@ func (r *ArgoCDRoleBindingReconciler) Reconcile(ctx context.Context, req ctrl.Re
 		if err := r.Client.Status().Update(ctx, &rb); err != nil {
 			r.Log.Error(err, "Failed to update ArgoCDRoleBinding %s status", req.Name)
 		}
-		return ctrl.Result{RequeueAfter: time.Minute * 10}, nil
+		return ctrl.Result{RequeueAfter: time.Second}, nil
 
 	}
 
